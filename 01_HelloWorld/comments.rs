3b1bc1db7be0300d7c94c928798af36f22794912
Alex Pakalniskis 3 <alexpakalniskis@gmail.com>
2022-09-07 09:03:36 -0700

    First session code

---
+++ 01_HelloWorld/comments.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // This is a standard comment we're used to
+    
+    /* 
+    This comment is a little more fancy 
+
+    We can go multi-line
+    
+    
+    
+    */
+
+    let x = 5 + /*90 +  */  5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+}
+
---
+++ 01_HelloWorld/hello.rs
@@ -0,0 +1,9 @@
+// Code Alex with Alex
+// That's it for now
+
+
+fn main() {
+    // A comment in a function
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
